c1db39376ab5134b0a761c1c04dc082c52330bde
Apurba-K <apurba_git@yahoo.com>
2018-07-14 21:34:54 +0530

    Update BiasedMutex.h

--- BiasedMutex.h
+++ BiasedMutex.h
@@ -1,22 +1,33 @@
 #pragma once
 #include <atomic>
+#include <vector>
+#include <memory>
 
 class BiasedMutex
 {
 	int maxAllowedBias = 4;                         // Max generocity to be shown by low priority job
 	enum LOCKSTATE :int { LOCKED = -1, FREE };
-	
-	std::atomic< int > lockState1, lockState2;      // Current state lockable
-	std::atomic< int > priorityWaitCount;           // Depth of pending and continuing priority requests  at present
+
+	struct LockState
+	{
+		std::atomic< int > state;
+		std::atomic< int > waitCount;
+	};
+		
+	int maxPriority;
+	std::vector< std::shared_ptr<LockState> >lockStates;
+
+	void setMaxPriority(int maxp);
+	void lock(int lockPriority, int reqPriority);
+
 public:
-	enum class BIAS :int { HIGHEST, LOW };          // Biased on which lock is required
-
-	BiasedMutex();
+	
+	BiasedMutex(int maxp = 2);
 	BiasedMutex(BiasedMutex&& biasedmutex);
-	BiasedMutex(const BiasedMutex& biasedmutex);
-
-	void lock(BIAS bias);
-	void unlock(BIAS bias);
+	BiasedMutex(const BiasedMutex& biasedmutex) = delete;
+	
+	void lock(int priority);
+	void unlock(int priority);
 
 	void setMaxAllowedBias(int maxbais);
 };
