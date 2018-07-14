fafbf9dbc4d3811278103d1cbaddfd646a6c96e0
Apurba-K <apurba_git@yahoo.com>
2018-07-14 21:37:32 +0530

    Update biasedMutex.cpp

--- biasedMutex.cpp
+++ biasedMutex.cpp
@@ -3,73 +3,81 @@
 #include "BiasedMutex.h"
 
 
-BiasedMutex::BiasedMutex() : lockState1(LOCKSTATE::FREE), lockState2(LOCKSTATE::FREE), priorityWaitCount(0)
-{}
+BiasedMutex::BiasedMutex(int maxp)
+{
+	setMaxPriority(maxp);
+}
 
-BiasedMutex::BiasedMutex(BiasedMutex&& biasedmutex) : lockState1(static_cast<int>(biasedmutex.lockState1)),
-                                                      lockState2(static_cast<int>(biasedmutex.lockState2)),
-                                                      priorityWaitCount(static_cast<int>(biasedmutex.priorityWaitCount))
-{}
-BiasedMutex::BiasedMutex(const BiasedMutex& biasedmutex) : lockState1(LOCKSTATE::FREE),
-                                                           lockState2(LOCKSTATE::FREE), priorityWaitCount(0)
-{}
+
+BiasedMutex::BiasedMutex(BiasedMutex&& biasedmutex)
+{
+	maxPriority = biasedmutex.maxPriority;
+	lockStates = std::move(biasedmutex.lockStates);
+}
+
 
 void BiasedMutex::setMaxAllowedBias(int maxbais)
 {
 	maxAllowedBias = maxbais;
 }
 
-void BiasedMutex::lock(BIAS bias)
+void BiasedMutex::setMaxPriority(int maxp)
 {
-	int curVal = LOCKSTATE::FREE, writeVal = LOCKSTATE::LOCKED, waitCountForLowBias = 0;
-	bool marked = false;
-
-	//A low priority request need to get primary lock
-	//Because priorty work does not need to compete with all low priority work
-	
-	if (bias == BIAS::LOW)
+	maxPriority = maxp;
+	for (int count = 0; count < maxp; count++)
 	{
-		while ( !lockState1.compare_exchange_weak(curVal, writeVal) )
+		lockStates.push_back(std::make_shared<LockState>());
+		lockStates[count]->state = LOCKSTATE::FREE;
+		lockStates[count]->waitCount = 0;
+	}
+	return;
+}
+
+void BiasedMutex::lock(int lockPriority, int reqPriority)
+{
+	int curVal = LOCKSTATE::FREE, writeVal = LOCKSTATE::LOCKED, waitCount = 0;
+	
+	// One of the thread with same priority will get to proceed
+	if (lockPriority == reqPriority)
+	{
+		lockStates[lockPriority]->waitCount++;
+		while ( !lockStates[lockPriority]->state.compare_exchange_weak(curVal, writeVal) )
 		{
 			std::this_thread::yield();
 			curVal = LOCKSTATE::FREE;
 		}
+		lockStates[lockPriority]->waitCount--;
 	}
-	
-	curVal = LOCKSTATE::FREE, writeVal = LOCKSTATE::LOCKED;
-	// A request with low priority can be successful only when there is no pending high priority task
-
-	while ( (bias == BIAS::LOW && priorityWaitCount != 0) || !lockState2.compare_exchange_weak(curVal, writeVal) )
+	else
 	{
-		if (bias == BIAS::HIGHEST && !marked)
+		// When lower priorit thread compete with Higher priority thread,Higher priority thread will get the chance to proceed
+		// To avoid startvation, Lower priority thread will promote to higher priority after max try
+		while (((lockPriority > reqPriority) && lockStates[lockPriority]->waitCount != 0) || !lockStates[lockPriority]->state.compare_exchange_weak(curVal, writeVal))
 		{
-			// A high priority task is waiting for permission to proceed
-			priorityWaitCount++;
-			marked = true;
+			std::this_thread::yield();
+			if (lockPriority > reqPriority)
+			{
+				waitCount++;   // Low priority task should not suffer limitless
+				if (waitCount > maxAllowedBias)   // After max generocity low priority task should be treated same like high priority task
+					reqPriority = lockPriority;
+			}
+			curVal = LOCKSTATE::FREE;
 		}
-		else if (bias == BIAS::LOW)
-			waitCountForLowBias++;                 // Low priority task should not suffer limitless
-		
-		std::this_thread::yield();
-
-		if (waitCountForLowBias > maxAllowedBias)   // After max generocity low priority task should be treated same like high priority task
-			bias = BIAS::HIGHEST;
-		
-		curVal = LOCKSTATE::FREE;
 	}
-
-	if (marked)
-		priorityWaitCount--;
-
 	return;
 }
 
-void BiasedMutex::unlock(BIAS bias)
+void BiasedMutex::lock(int priority)
 {
-	if(bias == BIAS::HIGHEST)
-		lockState2 = LOCKSTATE::FREE;
-	else
-	{
-		lockState1 = lockState2 = LOCKSTATE::FREE;
-	}
+	for (int prio = 0; prio < maxPriority; prio++)
+		if (prio >= priority - 1)
+			lock(prio, priority - 1);
+	return;
+}
+
+void BiasedMutex::unlock(int priority)
+{
+	for (int prio = priority - 1; prio < maxPriority; prio++)
+		lockStates[prio]->state = LOCKSTATE::FREE;
+	return;
 }
